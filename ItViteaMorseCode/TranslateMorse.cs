41ac13b43d6e4d9ea07c17aa3d012893c2b39d1b
Stella van der burg <luna4s108@gmail.com>
2020-05-19 11:45:48 +0200

    Beginning building foundation of program. Added basic ViewModel, beginning of Command interface and the TranslateMorse class to handle all the conversions.

---
+++ ItViteaMorseCode/TranslateMorse.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItViteaMorseCode
+{
+    public class TranslateMorse
+    {
+
+        public TranslateMorse()
+        {
+
+        }
+
+        #region static dictionaries
+        private static readonly Dictionary<char, string> dictToMorse = new Dictionary<char, string>
+        {
+            { 'A', ".-" },
+            { 'B', "-..." },
+            { 'C', "-.-." },
+            { 'D', "-.." },
+            { 'E', "." },
+            { 'F', "..-." },
+            { 'G', "--." },
+            { 'H', "...." },
+            { 'I', ".." },
+            { 'J', ".---" },
+            { 'K', "-.-" },
+            { 'L', ".-.." },
+            { 'M', "--" },
+            { 'N', "-." },
+            { 'O', "---" },
+            { 'P', ".--." },
+            { 'Q', "--.-" },
+            { 'R', ".-." },
+            { 'S', "..." },
+            { 'T', "-" },
+            { 'U', "..-" },
+            { 'V', "...-" },
+            { 'W', ".--" },
+            { 'X', "-..-" },
+            { 'Y', "-.--" },
+            { 'Z', "--.." },
+            { '1', ".----" },
+            { '2', "..---" },
+            { '3', "...--" },
+            { '4', "....-" },
+            { '5', "....." },
+            { '6', "-...." },
+            { '7', "--..." },
+            { '8', "---.." },
+            { '9', "----." },
+            { '0', "-----" },
+            { ' ', " " },
+        };
+        #endregion
+
+        public string ToMorse(string input)
+        {
+            string output = "", value = "";
+            //Changes input string to uppercase, then array so it can be used to get dictionary entries.
+            char[] charArr = input.ToUpper().ToCharArray();
+
+            //For each char in the array check if there is a key in the dictionary and add that to the output string.
+            //If the key can't be found, put # instead for unrecognised char. (To prevent crashing if the user adds unrecognisable characters.)
+            foreach (char c in charArr)
+            {
+                if (dictToMorse.TryGetValue(c, out value))
+                    output += value + " ";
+                else
+                    output += "#" + " ";
+            }
+            return output;
+        }
+    }
+}
---
+++ ItViteaMorseCode/ViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItViteaMorseCode
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        //private variables for public properties.
+        private string _TextBox1, _TextBox2;
+
+        public ViewModel()
+        {
+
+        }
+
+        #region public properties
+        public string TextBox1
+        {
+            get { return _TextBox1; }
+            set
+            {
+                _TextBox1 = value;
+                OnPropertyChanged();
+            }
+        }
+        public string TextBox2
+        {
+            get { return _TextBox2; }
+            set
+            {
+                _TextBox2 = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
